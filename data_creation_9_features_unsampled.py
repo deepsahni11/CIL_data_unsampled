3818c56a27e41f213e58b3478094ffcc87235460
deepsahni11 <na16b110@smail.iitm.ac.in>
2020-03-20 03:51:31 +0530

    Update data_creation_9_features_unsampled.py

--- data_creation_9_features_unsampled.py
+++ data_creation_9_features_unsampled.py
@@ -102,7 +102,7 @@ for w in weights:
 #         break
                         
 
-np.save('../Data_metrics_9d_datasets_X_train_unsampled.npy',X_train_datasets_9d_unsampled)
-np.save('../Data_metrics_9d_datasets_X_test_unsampled.npy',X_test_datasets_9d_unsampled)
-np.save('../Data_metrics_9d_datasets_y_train_unsampled.npy',y_train_datasets_9d_unsampled)
-np.save('../Data_metrics_9d_datasets_y_test_unsampled.npy',y_test_datasets_9d_unsampled)
+np.save('../datasets_9d_X_train_unsampled.npy',X_train_datasets_9d_unsampled)
+np.save('../datasets_9d_X_test_unsampled.npy',X_test_datasets_9d_unsampled)
+np.save('../datasets_9d_y_train_unsampled.npy',y_train_datasets_9d_unsampled)
+np.save('../datasets_9d_y_test_unsampled.npy',y_test_datasets_9d_unsampled)
